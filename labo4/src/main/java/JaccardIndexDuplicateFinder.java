57b1f687e0a84ad663d70ed3e6ee2fc0e8a58458
Jarno Willems <jarnowillems001@gmail.com>
2026-03-23 15:22:05 +0100

    Labo 4.3 done

--- labo4/src/main/java/JaccardIndexDuplicateFinder.java
+++ labo4/src/main/java/JaccardIndexDuplicateFinder.java
@@ -13,8 +13,31 @@ public class JaccardIndexDuplicateFinder extends DuplicateFinder{
     }
 
     @Override
-    public List<Pair> findDuplicates(double threshold) {
-        return new ArrayList<>();
+    public List<Pair> findDuplicates(double threshold) { // alleen returen wat boven de threshold komt
+        ArrayList<Pair> pairs = new ArrayList<>();
+
+
+        for (int i = 0; i < tokens.size(); i++){
+            for (int j = i + 1 ; j < tokens.size(); j++){
+                double jac = jaccard(tokens.get(i), tokens.get(j));
+                if ( jac > threshold){
+                    pairs.add(new Pair(i, j, jac));
+                }
+            }
+        }
+
+        return pairs;
     }
-    
+
+    private double jaccard(Set<String> A, Set<String> B){
+        int AnB = 0;
+
+        for(String s : A){
+            if (B.contains(s)){
+                AnB++;
+            }
+        }
+
+        return (double) (AnB) / (A.size() + B.size() - AnB);
+    }
 }
